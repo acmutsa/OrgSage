d84010855d38119ff2440b2ec661eeb35b9312e4
xwilson03 <xanderwilson2014@gmail.com>
2024-11-03 10:40:32 -0600

    commits partial invites page

--- src/app/dash/invites/page.tsx
+++ src/app/dash/invites/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { acceptInviteAction, deleteInviteAction, getUserInvitesAction } from "@/app/actions/invites";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
@@ -7,32 +10,57 @@ import {
     CarouselNext,
     CarouselPrevious
 } from "@/components/ui/carousel";
+import { selectInviteSchema } from "@/db/zod";
+import { useState } from "react";
 
-const temp = [
-    "blah1",
-    "blah2",
-    "blah3",
-    "blah4",
-    "blah5",
-    "blah6",
-]
+export default async function Page() {
 
-export default function Page() {
+    const [invites, setInvites] = useState([{}]);
+
+    const handleAccept = (id_: number) => {
+        acceptInviteAction({id: id_});
+        invites.splice(invites.findIndex(({id}) => {id == id_}), 1)
+    }
+
+    const handleDeny = (id: number) => {
+        deleteInviteAction({id});
+    }
+
+    // const invites = (await getUserInvitesAction())?.data;
+    setInvites([
+        {id: 1, orgs: {orgName: "skibidi"}},
+        {id: 2, orgs: {orgName: "aaaaaaaaaaaaaaaaaaaaaaaaaaaa"}},
+        {id: 3, orgs: {orgName: "notverylong"}},
+        {id: 4, orgs: {orgName: "ACM-UTSA"}}
+    ]);
+
+    if (!invites) return (
+        <div className="align-center flex w-full flex-grow justify-center p-20 text-2xl text-white">
+            <p> No invites.</p>
+        </div>
+    );
+
     return (
         <div className="align-center flex w-full flex-grow justify-center p-20 text-white">
             <Carousel className="flex h-1/2 w-[20em] items-center">
                 <CarouselContent>
-                    {temp.map((text) => { return (
-                        <CarouselItem key={text} >
+                    {invites.map((invite) => { return (
+                        <CarouselItem key={invite.id} >
                             <Card className="flex h-full flex-col">
                                 <CardHeader className="text-xl">
                                     Invited to...
                                 </CardHeader>
-                                <CardContent className="flex flex-col gap-y-6 text-center text-2xl">
-                                    <p> Organization Name </p>
+                                <CardContent className="flex flex-col gap-y-6 text-2xl">
+                                    <p className="overflow-clip text-ellipsis text-center">
+                                        {invite.orgs.orgName}
+                                    </p>
                                     <div className="flex flex-row justify-around">
-                                        <Button> Accept </Button>
-                                        <Button> Deny </Button>
+                                        <Button onClick={() => handleAccept(invite.id)}>
+                                            Accept
+                                        </Button>
+                                        <Button onClick={() => handleDeny(invite.id)}>
+                                            Deny
+                                        </Button>
                                     </div>
                                 </CardContent>
                             </Card>
