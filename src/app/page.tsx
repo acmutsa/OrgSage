8fe0ec2915e9bf8e226dc047cba57070e741ca06
xwilson03 <xanderwilson2014@gmail.com>
2024-11-03 01:59:58 -0500

    adds redirect button to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+import { Button } from "@/components/ui/button";
 import Navbar from "../components/navbar";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col text-white">
       <Navbar/>
-      <div className="flex-grow p-8 mt-10">
-        <h2 className="text-4xl font-bold text-center">Welcome to OrgSage</h2>
-        <p className="text-lg text-white text-center">
-          OrgSage is your single source of truth for organizational knowledge.
-        </p>
+      <div className="mt-10 flex flex-grow flex-col items-center gap-y-6 p-8">
+        <div className="flex flex-col items-center gap-y-2">
+            <h2 className="text-center text-4xl font-bold">Welcome to OrgSage</h2>
+            <p className="text-center text-lg text-white">
+            OrgSage is your single source of truth for organizational knowledge.
+            </p>
+        </div>
+        <Link href="/dash">
+            <Button>
+                Get Started
+            </Button>
+        </Link>
       </div>
   </div>
   );
