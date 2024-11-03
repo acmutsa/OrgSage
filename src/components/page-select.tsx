8e9821ab692de4a83f288e71df22930fc53b0499
xwilson03 <xanderwilson2014@gmail.com>
2024-11-03 01:11:34 -0600

    upgrades page select component

--- src/components/page-select.tsx
+++ src/components/page-select.tsx
@@ -1,18 +1,17 @@
-import Logo from "./home-logo";
+import Link from "next/link";
+
+const links = ["Chatbots", "Invites"]
 
 const PageSelect = () => {
   return (
-    <div className="flex justify-between border-b border-white bg-zinc-950 px-4 py-3">
-      <div className="flex items-center gap-x-3">
-        <Logo/>
-        <span className="text-2xl font-semibold text-white">OrgSage</span>
-      </div>
-      
-      <div className="ml-auto flex items-center p-2">
-        <Logo/>
-        <span className="text-2xl font-semibold text-white">OrgSage</span>
-      </div>
-
+    <div className="flex gap-x-10 border-b border-white bg-zinc-950 p-4 pb-2 pt-6">
+        {links.map((linkName) => {
+            return (
+                <Link key={linkName} href={linkName.toLocaleLowerCase()} className="text-xl font-semibold text-white">
+                    {linkName}
+                </Link>
+            )
+        })}
     </div>
   );
 };
