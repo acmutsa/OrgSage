a44457757a6b56e4ab4b0290767d932ceb91ee12
xwilson03 <xanderwilson2014@gmail.com>
2024-11-03 01:38:28 -0600

    changes dash spacing

--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -1,12 +1,9 @@
 export default function Page() {
   return (
-    <div className="flex flex-col text-white">
-      <div className="mt-10 flex-grow p-8">
-        <h2 className="text-center text-4xl font-bold">Welcome to OrgSage</h2>
-        <p className="text-center text-lg text-white">
-          OrgSage is your single source of truth for organizational knowledge.
+    <div className="align-center flex flex-grow items-center justify-center p-8 text-white">
+        <p className="text-center text-2xl">
+            Click a link above to get started.
         </p>
-      </div>
-  </div>
+    </div>
   );
 }
