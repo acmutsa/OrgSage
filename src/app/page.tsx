bfabe0a7b6e0097de5874bba7d3ea88232126125
Lasya Yakkala <lasya.yakkala@gmail.com>
2024-11-03 00:14:18 -0500

    updates main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import Navbar from "../../components/navbar";
+import Navbar from "../components/navbar";
 
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex flex-col text-white p-3">
-      <div className="justify-between">
-        <Navbar/>
+    <div className="w-screen h-screen text-white">
+      <Navbar/>
+      <div className="w-full h-full p-8 mt-10">
+        <h2 className="text-4xl font-bold text-center">Welcome to OrgSage</h2>
+        <p className="text-lg text-white text-center">
+          OrgSage is your single source of truth for organizational knowledge.
+        </p>
       </div>
-      <h2 className="text-4xl font-bold text-sage-800 pt-20">Welcome to OrgSage</h2>
-      <p className="text-lg text-white">
-        OrgSage is your single source of truth for organizational knowledge.
-      </p>
   </div>
   );
 }
