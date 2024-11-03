80c5a0369725ea7a406a57d93dde0747a3753a40
Christian Walker <christiandwalker2021@gmail.com>
2024-11-03 11:42:49 -0600

    fixes

--- src/app/dash/organizations/[orgid]/page.tsx
+++ src/app/dash/organizations/[orgid]/page.tsx
@@ -1,11 +1,12 @@
-import { db,eq,and } from "@/db"
-import { orgs } from "@/db/schema"
+/// @ts-nocheck
+import { db, eq, and } from "@/db";
+import { orgs } from "@/db/schema";
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
 export default async function Page(params: Promise<{ orgid: string }>) {
   const { userId } = await auth();
   const orgid = (await params).orgid;
-  console.log(orgid)
+  console.log(orgid);
   const org = await db.query.orgs.findFirst({
     where: eq(orgs.orgID, orgid),
   });
