53bdd4122c464ed09d768d271b007325a57a9043
Lasya Yakkala <lasya.yakkala@gmail.com>
2024-11-03 00:32:26 -0500

    fixes page size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen text-white">
+    <div className="flex flex-col text-white">
       <Navbar/>
-      <div className="w-full h-full p-8 mt-10">
+      <div className="flex-grow p-8 mt-10">
         <h2 className="text-4xl font-bold text-center">Welcome to OrgSage</h2>
         <p className="text-lg text-white text-center">
           OrgSage is your single source of truth for organizational knowledge.
